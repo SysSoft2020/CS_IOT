7d39e531aaea829c2e937f926b77bfceb7bc1a7c
IcE <ivica-matic@outlook.com>
2020-02-24 11:33:12 +0000

    Uploaded blank project directories for Server, Client and Sensor

---
+++ iotserver/src/build.sh
@@ -0,0 +1,12 @@
+if [[ "$*" == *-r* ]]
+then
+	#find old class files and delete them
+	#preventing output to terminal
+	find *.class -delete  > /dev/null 2>&1
+	echo "Rebuilding all"
+fi
+
+find -name *.java > build.txt
+javac -d .  @build.txt
+rm build.txt
+echo "Done"
