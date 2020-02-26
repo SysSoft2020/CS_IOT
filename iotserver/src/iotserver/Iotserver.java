3390fd130b69d197a9060e4adab1006b5bb7e210
IcE <ivica-matic@outlook.com>
2020-02-26 17:54:30 +0000

    Made database connection

--- iotserver/src/iotserver/Iotserver.java
+++ iotserver/src/iotserver/Iotserver.java
@@ -5,15 +5,31 @@
  */
 package iotserver;
 
-/**
- *
- * @author ivica
- */
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Iotserver {
+ 
     public static void main(String[] args) {
-        DatabaseConnection conn = new DatabaseConnection();
-        conn.openDbFile("database");
-        
+        try {
+            Class.forName("org.sqlite.JDBC");
+            String dbURL = "jdbc:sqlite:database.db";
+            Connection conn = DriverManager.getConnection(dbURL);
+            if (conn != null) {
+                System.out.println("Connected to the database");
+                DatabaseMetaData dm = (DatabaseMetaData) conn.getMetaData();
+                System.out.println("Driver name: " + dm.getDriverName());
+                System.out.println("Driver version: " + dm.getDriverVersion());
+                System.out.println("Product name: " + dm.getDatabaseProductName());
+                System.out.println("Product version: " + dm.getDatabaseProductVersion());
+                conn.close();
+            }
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
-    
 }
