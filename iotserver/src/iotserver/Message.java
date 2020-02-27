8c87cff1274359272a2295c9764cb1d95afc80e6
IcE <ivica-matic@outlook.com>
2020-02-27 00:05:28 +0000

    Re implementing message class

--- iotserver/src/iotserver/Message.java
+++ iotserver/src/iotserver/Message.java
@@ -4,67 +4,81 @@
  * and open the template in the editor.
  */
 package iotserver;
-
+import org.json.simple.*;
+import org.json.simple.parser.*;
+import java.io.*;
+import java.net.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author ivica
  */
 public class Message {
-    boolean isAuthMessage = false;
-    boolean isAddFieldMessage = false;
-    boolean isRemoveFieldMessage = false;
-    boolean isAddWeatherStationToFieldMessage = false;
-    boolean isRemoveWeatherStationFromFieldMessage = false;
-    boolean isAddWeatherStationDataMessage = false;
-    
-    String username;
-    String password;
-    
-    String fieldName;
-    String fieldId;
-    String weatherStationName;
-    String weatherStationId;
-    String weatherStationData;
-    
-    double temperature;
-    double windSpeed;
-    double barometricPressure;
-    double relativeHumidity;
-    int airQuality;
-    
-    
-    public void setMessage_AuthUser(String username,String password){
-        this.isAuthMessage = true;
-        this.username = username;
-        this.password = password;
+    /* Parameters that are going to get passed to message, so message
+    can send itself just by being provided with socket parameters
+    */
+    final Socket socket;
+    final DataInputStream inputStream;
+    final DataOutputStream outputStream;
+    /*
+    All data that is sent between clients and servers will be 
+    distinguished using first element of json array.
+    */
+    JSONArray data = new JSONArray();
+
+    public Message(Socket s, DataInputStream dis, DataOutputStream dos) {
+        this.socket = s;
+        this.inputStream = dis;
+        this.outputStream = dos;
     }
-    
-    public void setMessage_AddFieldMessage(String fieldName, String fieldId){
-        this.isAddFieldMessage = true;
-        this.fieldName = fieldName;
-        this.fieldId = fieldId;  
+
+    public Message(JSONArray incomingData) {
+        this.data = incomingData;
+        this.socket = null;
+        this.inputStream = null;
+        this.outputStream = null;
     }
-    
-    public void setMessage_RemoveFieldMessage(String fieldName, String fieldId){
-        this.isRemoveFieldMessage = true;
-        this.fieldName = fieldName;
-        this.fieldId = fieldId;  
+
+    public Message(String incomingData) {
+        this.socket = null;
+        this.inputStream = null;
+        this.outputStream = null;
+        try {
+            JSONParser jsonParser = new JSONParser();
+            this.data.add(jsonParser.parse(incomingData));
+
+        } catch (ParseException ex) {
+            Logger.getLogger(Message.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
-    public void setMessage_AddWeatherStationtoField(String weatherStationName,String weatherStationId){
-        this.isAddWeatherStationToFieldMessage = true;
-        this.weatherStationName = weatherStationName;
-        this.weatherStationId = weatherStationId;
+
+    private void send() {
+
     }
-    
-    public void setMessage_RemoveWeatherStationfromField(String weatherStationName,String weatherStationId){
-        this.isAddWeatherStationToFieldMessage = true;
-        this.weatherStationName = weatherStationName;
-        this.weatherStationId = weatherStationId;
+
+    private boolean sendWithBooleanReturn() {
+        boolean result = false;
+        try {
+
+            try {
+                outputStream.writeUTF(data.toString());
+            } catch (IOException ex) {
+                Logger.getLogger(Message.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            result = inputStream.readBoolean();
+        } catch (IOException ex) {
+            Logger.getLogger(Message.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return result;
+
     }
-    
-    public void setMessage_weatherStationData(String weatherStationData){
-        this.isAddWeatherStationDataMessage = true;
-        this.weatherStationData = weatherStationData;
+
+    public boolean authUser(String username, String password) {
+        data.add("AUTHREQUEST");
+        data.add(username);
+        data.add(password);
+        return sendWithBooleanReturn();
     }
+
 }
