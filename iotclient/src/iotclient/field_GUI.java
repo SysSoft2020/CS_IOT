64b9f7bc59abfcaf5dbc53ebf1bdfe921b2db55d
Ivica <ivica-matic@outlook.com>
2020-04-04 20:41:06 +0100

    Refactoring gui to get data from hash maps

--- iotclient/src/iotclient/field_GUI.java
+++ iotclient/src/iotclient/field_GUI.java
@@ -4,26 +4,28 @@
  * and open the template in the editor.
  */
 package iotclient;
-import java.awt.*;
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import javax.swing.event.ListSelectionListener;
 
-/**
- *
- * @author Sami Ljimari
- */
+import java.util.*;
+import javax.swing.DefaultListModel;
+
 public class field_GUI extends javax.swing.JFrame {
+
+    HashMap<String, HashMap<String,Vector>> fields = new HashMap<String, HashMap<String,Vector>>();
 
     /**
      * Creates new form field_GUI
      */
     public field_GUI() {
         initComponents();
-        
-        String[] Fields = {"Field 1", "Field 2", "Field 3", "Field 4", "Field 5", "Field 6", "Field 7"}; //array of available fields
-        populateComboBox(Fields);  //populating the combobox with available Fields
-       
+        fields.put("Field 1", new HashMap<String,Vector>());
+        fields.put("Field 2", new HashMap<String,Vector>());
+        fields.put("Field 3", new HashMap<String,Vector>());
+        fields.get("Field 1").put("Weather station 1", new Vector());
+                fields.get("Field 1").put("Weather station 2", new Vector());
+
+
+        populateComboBox();  //populating the combobox with available Fields
+
     }
 
     /**
@@ -139,20 +141,23 @@ public class field_GUI extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-       
+
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         //exit button, exits the gui
         System.exit(0);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void fieldSelectBoxItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_fieldSelectBoxItemStateChanged
-        //whenever user switches the field, showDataInField triggers
-        showDataInField();
+        //whenever user switches the field, showWeatherStationsInField triggers
+        showWeatherStationsInField();
     }//GEN-LAST:event_fieldSelectBoxItemStateChanged
 
     private void sensorListMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_sensorListMouseClicked
         //on mouse click index of the clicked item is sent to showWeatherStationData function which displays the local data
-        showWeatherStationData();
+        System.out.print("Ola");
+                String indexOfWeatherStation = String.valueOf(sensorList.getModel().getElementAt(sensorList.getSelectedIndex()));
+        System.out.print(indexOfWeatherStation);
+
     }//GEN-LAST:event_sensorListMouseClicked
 
     /**
@@ -164,7 +169,7 @@ public class field_GUI extends javax.swing.JFrame {
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
-      
+
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
@@ -190,131 +195,154 @@ public class field_GUI extends javax.swing.JFrame {
             }
         });
     }
-    
+
     /**
      * appends the combobox with a up to date list of all available fields.
-     * 
-     * The function takes in a string array of all available fields and updates 
+     *
+     * The function takes in a string array of all available fields and updates
      * whenever a new field is added.
-     * 
-     * @param Fields  string array of fields.
+     *
+     * @param Fields string array of fields.
      */
-    public void populateComboBox(String[] Fields){
-        fieldSelectBox.setModel(new DefaultComboBoxModel<String>(Fields));
+    public void populateComboBox() {
+        Set<String> a = fields.keySet();
+        for (String x : a) {
+            fieldSelectBox.addItem(x);
+        }
+
     }
-    
-     /**
+
+    /**
      * Displays all Weather Stations located on the field user selects.
-     * 
-     * This method utilizes the jComboBox1ItemStateChanged, so whenever combo box value changes
-     * the function displays appropriate weather stations for selected field.
-     * 
-     * Use this function when you need to display all available weather stations for a field.
-     * 
+     *
+     * This method utilizes the jComboBox1ItemStateChanged, so whenever combo
+     * box value changes the function displays appropriate weather stations for
+     * selected field.
+     *
+     * Use this function when you need to display all available weather stations
+     * for a field.
+     *
      * @param String array containing names of available fields.
      */
-    
-    public void showDataInField(){
+    public void showWeatherStationsInField() {
         String selectedField = fieldSelectBox.getSelectedItem().toString();
         DefaultListModel dlm = new DefaultListModel();
+        
+        HashMap<String,Vector> x = fields.get(selectedField);
+        Set<String> a = x.keySet();
+        for (String c : a) {
+            dlm.addElement(c);
+        }
+        /*
         //ArrayList<String> field = new ArrayList<String>();
-        
-        String[] field1 = {"Weather Station 1","Weather Station 2","Weather Station 3","Weather Station 4"};
-        String[] field2 = {"Weather Station 1","Weather Station 2","Weather Station 3","Weather Station 4","Weather Station 5","Weather Station 6"};
-        String[] field3 = {"Weather Station 1","Weather Station 2","Weather Station 3"};
-        String[] field4 = {"Weather Station 1","Weather Station 2"};
+
+        String[] field1 = {"Weather Station 1", "Weather Station 2", "Weather Station 3", "Weather Station 4"};
+        String[] field2 = {"Weather Station 1", "Weather Station 2", "Weather Station 3", "Weather Station 4", "Weather Station 5", "Weather Station 6"};
+        String[] field3 = {"Weather Station 1", "Weather Station 2", "Weather Station 3"};
+        String[] field4 = {"Weather Station 1", "Weather Station 2"};
         String[] field5 = {"Weather Station 1"};
-        String[] field6 = {"Weather Station 1","Weather Station 2","Weather Station 3"};
-        String[] field7 = {"Weather Station 1","Weather Station 2"};
-        
-         if (selectedField == "Field 1"){
-            for (int i = 0; i < field1.length; i++)
-            dlm.addElement(field1[i]);
+        String[] field6 = {"Weather Station 1", "Weather Station 2", "Weather Station 3"};
+        String[] field7 = {"Weather Station 1", "Weather Station 2"};
+
+        if (selectedField == "Field 1") {
+            for (int i = 0; i < field1.length; i++) {
+                dlm.addElement(field1[i]);
+            }
         }
-        if (selectedField == "Field 2"){
-            for (int i = 0; i < field2.length; i++)
-            dlm.addElement(field2[i]);
+        if (selectedField == "Field 2") {
+            for (int i = 0; i < field2.length; i++) {
+                dlm.addElement(field2[i]);
+            }
         }
-        if (selectedField == "Field 3"){
-            for (int i = 0; i < field3.length; i++)
-            dlm.addElement(field3[i]);
+        if (selectedField == "Field 3") {
+            for (int i = 0; i < field3.length; i++) {
+                dlm.addElement(field3[i]);
+            }
         }
-        if (selectedField == "Field 4"){
-            for (int i = 0; i < field4.length; i++)
-            dlm.addElement(field4[i]);
+        if (selectedField == "Field 4") {
+            for (int i = 0; i < field4.length; i++) {
+                dlm.addElement(field4[i]);
+            }
         }
-        if (selectedField == "Field 5"){
-            for (int i = 0; i < field5.length; i++)
-            dlm.addElement(field5[i]);
+        if (selectedField == "Field 5") {
+            for (int i = 0; i < field5.length; i++) {
+                dlm.addElement(field5[i]);
+            }
         }
-        if (selectedField == "Field 6"){
-            for (int i = 0; i < field6.length; i++)
-            dlm.addElement(field6[i]);
+        if (selectedField == "Field 6") {
+            for (int i = 0; i < field6.length; i++) {
+                dlm.addElement(field6[i]);
+            }
         }
-        if (selectedField == "Field 7"){
-            for (int i = 0; i < field7.length; i++)
-            dlm.addElement(field7[i]);
+        if (selectedField == "Field 7") {
+            for (int i = 0; i < field7.length; i++) {
+                dlm.addElement(field7[i]);
+            }
         }
-        sensorList.setModel(dlm); 
+        */
+        sensorList.setModel(dlm);
     }
-    
-    
+
     /**
      * Displays Weather Station data according to unique index.
-     * 
-     * This method utilizes the mouse click event, takes the current selected index from JList
-     * and matches it with the appropriate string array to display in Text Area.
-     * 
-     * Use this function when you need to display measurements made by a Weather Station 
-     * on the specified field.
+     *
+     * This method utilizes the mouse click event, takes the current selected
+     * index from JList and matches it with the appropriate string array to
+     * display in Text Area.
+     *
+     * Use this function when you need to display measurements made by a Weather
+     * Station on the specified field.
      */
-    
-    public void showWeatherStationData(){
-        
+    public void showWeatherStationData() {
+
         String indexOfWeatherStation = String.valueOf(sensorList.getModel().getElementAt(sensorList.getSelectedIndex()));
-        
-        String[] data1 = {"Weather Station 1: \n","weather index: 12 \n","erosion: 12%\n", "humidity: 4%\n", "chance of rain: 0%\n"};
-        String[] data2 = {"Weather Station 2: \n","weather index: 11 \n","erosion: 22%\n", "humidity: 34%\n", "chance of rain: 12%\n"};
-        String[] data3 = {"Weather Station 3: \n","weather index: 22 \n","erosion: 9%\n", "humidity: 30%\n", "chance of rain: 99%\n"};
-        String[] data4 = {"Weather Station 4: \n","weather index: 7 \n","erosion: 97%\n", "humidity: 42%\n", "chance of rain: 60%\n"};
-        String[] data5 = {"Weather Station 5: \n","weather index: 2 \n","erosion: 33%\n", "humidity: 57%\n", "chance of rain: 20%\n"};
-        String[] data6 = {"Weather Station 6: \n","weather index: 100 \n","erosion: 18%\n", "humidity: 2%\n", "chance of rain: 100%\n"};
-        
-        if (indexOfWeatherStation == "Weather Station 1"){
-            for (int i = 0; i < data1.length;i++)
-            sensorDataField.append(data1[i]);
+
+        String[] data1 = {"Weather Station 1: \n", "weather index: 12 \n", "erosion: 12%\n", "humidity: 4%\n", "chance of rain: 0%\n"};
+        String[] data2 = {"Weather Station 2: \n", "weather index: 11 \n", "erosion: 22%\n", "humidity: 34%\n", "chance of rain: 12%\n"};
+        String[] data3 = {"Weather Station 3: \n", "weather index: 22 \n", "erosion: 9%\n", "humidity: 30%\n", "chance of rain: 99%\n"};
+        String[] data4 = {"Weather Station 4: \n", "weather index: 7 \n", "erosion: 97%\n", "humidity: 42%\n", "chance of rain: 60%\n"};
+        String[] data5 = {"Weather Station 5: \n", "weather index: 2 \n", "erosion: 33%\n", "humidity: 57%\n", "chance of rain: 20%\n"};
+        String[] data6 = {"Weather Station 6: \n", "weather index: 100 \n", "erosion: 18%\n", "humidity: 2%\n", "chance of rain: 100%\n"};
+
+        if (indexOfWeatherStation == "Weather Station 1") {
+            for (int i = 0; i < data1.length; i++) {
+                sensorDataField.append(data1[i]);
+            }
             sensorDataField.append("\n");
         }
-        if (indexOfWeatherStation == "Weather Station 2"){
-            for (int i = 0; i < data2.length;i++)
-            sensorDataField.append(data2[i]);
+        if (indexOfWeatherStation == "Weather Station 2") {
+            for (int i = 0; i < data2.length; i++) {
+                sensorDataField.append(data2[i]);
+            }
             sensorDataField.append("\n");
         }
-        if (indexOfWeatherStation == "Weather Station 3"){
-            for (int i = 0; i < data3.length;i++)
-            sensorDataField.append(data3[i]);
+        if (indexOfWeatherStation == "Weather Station 3") {
+            for (int i = 0; i < data3.length; i++) {
+                sensorDataField.append(data3[i]);
+            }
             sensorDataField.append("\n");
         }
-        if (indexOfWeatherStation == "Weather Station 4"){
-            for (int i = 0; i < data4.length;i++)
-            sensorDataField.append(data4[i]);
+        if (indexOfWeatherStation == "Weather Station 4") {
+            for (int i = 0; i < data4.length; i++) {
+                sensorDataField.append(data4[i]);
+            }
             sensorDataField.append("\n");
         }
-        if (indexOfWeatherStation == "Weather Station 5"){
-            for (int i = 0; i < data5.length;i++)
-            sensorDataField.append(data5[i]);
+        if (indexOfWeatherStation == "Weather Station 5") {
+            for (int i = 0; i < data5.length; i++) {
+                sensorDataField.append(data5[i]);
+            }
             sensorDataField.append("\n");
         }
-        if (indexOfWeatherStation == "Weather Station 6"){
-            for (int i = 0; i < data6.length;i++)
-            sensorDataField.append(data6[i]);
+        if (indexOfWeatherStation == "Weather Station 6") {
+            for (int i = 0; i < data6.length; i++) {
+                sensorDataField.append(data6[i]);
+            }
             sensorDataField.append("\n");
         }
     }
-    
 
-     
-    
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JComboBox<String> fieldSelectBox;
     private javax.swing.JButton jButton2;
